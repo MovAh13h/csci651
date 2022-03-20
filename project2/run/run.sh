6bac81750ed86c72306be29d2dfb104202fd4a7e
Tanishq Jain <tan@Tanishqs-MacBook-Pro.local>
2022-03-20 18:16:34 -0400

    123

--- project2/run/run.sh
+++ project2/run/run.sh
@@ -6,6 +6,23 @@
 #first, start the nodejs server to receive any initial commands
 node run/run.js $1 &
 
+sleep 5
+
+if [ $1 -eq 1 ]
+then
+	curl "http://localhost:8080/?block=172.18.0.23&indrop=0.2"
+fi
+
+if [ $1 -eq 2 ]
+then
+	curl "http://localhost:8080/?indrop=0.2"
+fi
+
+if [ $1 -eq 3 ]
+then
+	curl "http://localhost:8080/?block=172.18.0.21&indrop=0.2"
+fi
+
 #then wait 10 seconds to make sure the commands came and executed
 sleep 10
 
